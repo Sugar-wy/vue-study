2300b8133c5f4f17062c4cabff2b0460eb7071cf
dell <1836477214@qq.com>
2021-05-06 16:55:17 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
+import store from "./store";
 
-Vue.config.productionTip = false
+// 1.导入 ant-design-vue 组件库
+import Antd from 'ant-design-vue'
+// 2.导入组件库的样式表
+import 'ant-design-vue/dist/antd.css'
+
+Vue.config.productionTip = false;
+
+Vue.use(Antd);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app');
